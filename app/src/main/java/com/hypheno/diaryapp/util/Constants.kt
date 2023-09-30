9127b7ed406192d6b14da9f7dd65a443896decf4
Vivek <vivek.sharma@mpokket.com>
2023-09-30 16:46:19 +0530

    Navigation added

---
+++ app/src/main/java/com/hypheno/diaryapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.hypheno.diaryapp.util
+
+object Constants {
+
+    const val APP_ID = "application-0-gjwoj"
+    const val CLIENT_ID = "886659748099-bdin4f29l96nuqk6ssf9kmdusl8sumj3.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
