b0cd0a43664c4a816b322dafe43ea8bb16402eb4
Vivek <vivek.sharma@mpokket.com>
2023-10-01 12:38:25 +0530

    NavigationDrawer added

--- app/src/main/java/com/hypheno/diaryapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hypheno/diaryapp/presentation/screens/home/HomeScreen.kt
@@ -1,33 +1,107 @@
 package com.hypheno.diaryapp.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hypheno.diaryapp.R
 import com.hypheno.diaryapp.ui.theme.DiaryAppTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
+    drawerState: DrawerState,
     onMenuClicked: () -> Unit,
     navigateToWrite: () -> Unit,
+    onSignOutClicked: () -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            HomeTopBar(onMenuClicked = onMenuClicked)
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Default.Edit, contentDescription = "New")
+    NavigationDrawer(
+        drawerState = drawerState,
+        onSignOutClicked = onSignOutClicked
+    ) {
+        Scaffold(
+            topBar = {
+                HomeTopBar(onMenuClicked = onMenuClicked)
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = navigateToWrite) {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = "New")
+                }
+            },
+            content = {
+
+            }
+        )
+    }
+}
+
+@Composable
+fun NavigationDrawer(
+    drawerState: DrawerState,
+    onSignOutClicked: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier
+                    .width(300.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        modifier = Modifier.size(250.dp),
+                        painter = painterResource(id = R.drawable.ic_logo),
+                        contentDescription = "Diary"
+                    )
+                }
+                NavigationDrawerItem(
+                    label = {
+                        Row(modifier = Modifier.padding(horizontal = 12.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_google_logo),
+                                contentDescription = "Google Logo"
+                            )
+                            Spacer(modifier = Modifier.width(12.dp))
+                            Text(text = "Sign Out", color = MaterialTheme.colorScheme.onSurface)
+                        }
+                    },
+                    selected = false,
+                    onClick = onSignOutClicked
+                )
             }
         },
-        content = {
-
-        }
+        content = content
     )
 }
 
@@ -36,8 +110,10 @@ fun HomeScreen(
 private fun Preview() {
     DiaryAppTheme {
         HomeScreen(
+            drawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
             onMenuClicked = {},
             navigateToWrite = {},
+            onSignOutClicked = {}
         )
     }
 }
