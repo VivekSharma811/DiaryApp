ba629b5ab8cb832d9f54892e531bae5f5e5c9d4f
Vivek <vivek.sharma@mpokket.com>
2023-10-01 12:08:31 +0530

    HomeTopBar added

--- app/src/main/java/com/hypheno/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/hypheno/diaryapp/navigation/NavGraph.kt
@@ -19,6 +19,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.hypheno.diaryapp.presentation.screens.auth.AuthenticationScreen
 import com.hypheno.diaryapp.presentation.screens.auth.AuthenticationViewModel
+import com.hypheno.diaryapp.presentation.screens.home.HomeScreen
 import com.hypheno.diaryapp.util.Constants.APP_ID
 import com.hypheno.diaryapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 import com.stevdzasan.messagebar.rememberMessageBarState
@@ -34,7 +35,9 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
             navController.popBackStack()
             navController.navigate(Screen.Home.route)
         }
-        homeRoute()
+        homeRoute(navigateToWrite = {
+            navController.navigate(Screen.Write.route)
+        })
         writeRoute()
     }
 }
@@ -78,22 +81,14 @@ fun NavGraphBuilder.authenticationRoute(
     }
 }
 
-fun NavGraphBuilder.homeRoute() {
+fun NavGraphBuilder.homeRoute(
+    navigateToWrite: () -> Unit
+) {
     composable(route = Screen.Home.route) {
-        val scope = rememberCoroutineScope()
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = {
-                scope.launch(Dispatchers.IO) {
-                    App.create(APP_ID).currentUser?.logOut()
-                }
-            }) {
-                Text(text = "Logout")
-            }
-        }
+        HomeScreen(
+            onMenuClicked = {},
+            navigateToWrite = navigateToWrite,
+        )
     }
 }
 
---
+++ app/src/main/java/com/hypheno/diaryapp/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,52 @@
+package com.hypheno.diaryapp.presentation.screens.home
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.hypheno.diaryapp.ui.theme.DiaryAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(onMenuClicked: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = onMenuClicked) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        },
+        title = {
+            Text(text = "Diary")
+        },
+        actions = {
+            IconButton(onClick = onMenuClicked) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Date",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    DiaryAppTheme {
+        HomeTopBar {
+
+        }
+    }
+}
